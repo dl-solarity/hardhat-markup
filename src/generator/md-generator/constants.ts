daba49658f981141f1c5cedce333fe43522e6abf
oleg.komendant <oleg02kom@gmail.com>
2023-01-19 17:57:34 +0200

    Update md files style

--- src/generator/md-generator/constants.ts
+++ src/generator/md-generator/constants.ts
@@ -1,5 +1,13 @@
+export enum AlignTypes {
+  CENTER = "center",
+  RIGHT = "right",
+  LEFT = "left",
+  NONE = "none",
+}
+
 export const CONTRACT_NAME_H_SIZE = 1;
 export const TOPIC_H_SIZE = 2;
 export const FUNCTION_NAME_H_SIZE = 3;
 export const FUNCTION_INSIDE_H_SIZE = 4;
 export const DEFAULT_CODE_LANGUAGE = "js";
+export const DEFAULT_TABLE_ALIGN = AlignTypes.LEFT;
--- src/generator/md-generator/md-generator.ts
+++ src/generator/md-generator/md-generator.ts
@@ -11,9 +11,10 @@ import {
   Return,
 } from "../../parser/types";
 import {
+  AlignTypes,
   CONTRACT_NAME_H_SIZE,
   DEFAULT_CODE_LANGUAGE,
-  FUNCTION_INSIDE_H_SIZE,
+  DEFAULT_TABLE_ALIGN,
   FUNCTION_NAME_H_SIZE,
   TOPIC_H_SIZE,
 } from "./constants";
@@ -26,10 +27,12 @@ class MDGenerator {
 
     contractTags.push(this.createHeaderTag(contractInfo.name, CONTRACT_NAME_H_SIZE));
 
-    contractTags.push(this.createHeaderTag("Contract Description", TOPIC_H_SIZE));
+    if (contractInfo.notice || contractInfo.details || contractInfo.author) {
+      contractTags.push(this.createHeaderTag("Contract Description", TOPIC_H_SIZE));
+    }
 
     if (contractInfo.author) {
-      contractTags.push(this.createParagraphTag(`${this.getBoldStr("Author:")} ${contractInfo.author}`));
+      contractTags.push(this.createParagraphTag(`Authored by ${contractInfo.author}`));
     }
 
     this.generateBaseDescriptionBlock(contractTags, contractInfo);
@@ -37,60 +40,65 @@ class MDGenerator {
     const events: EventsInfo | undefined = contractInfo.events;
 
     if (events) {
-      contractTags.push(this.createHeaderTag("Events info", TOPIC_H_SIZE));
+      const keys: string[] = Object.keys(events);
 
-      Object.keys(events).forEach((eventSign: string) => {
-        this.generateEventBlock(contractTags, eventSign, events[eventSign]);
-      });
+      if (keys.length > 0) {
+        contractTags.push(this.createHeaderTag("Events info", TOPIC_H_SIZE));
+
+        keys.forEach((eventSign: string) => {
+          this.generateEventBlock(contractTags, eventSign, events[eventSign]);
+        });
+      }
     }
 
     const errors: ErrorsInfo | undefined = contractInfo.errors;
 
     if (errors) {
-      contractTags.push(this.createHeaderTag("Errors info", TOPIC_H_SIZE));
+      const keys: string[] = Object.keys(errors);
 
-      Object.keys(errors).forEach((errorSign: string) => {
-        this.generateErrorBlock(contractTags, errorSign, errors[errorSign]);
-      });
+      if (keys.length > 0) {
+        contractTags.push(this.createHeaderTag("Errors info", TOPIC_H_SIZE));
+
+        keys.forEach((errorSign: string) => {
+          this.generateErrorBlock(contractTags, errorSign, errors[errorSign]);
+        });
+      }
     }
 
     const functions: FunctionsInfo | undefined = contractInfo.functions;
 
     if (functions) {
-      contractTags.push(this.createHeaderTag("Functions info", TOPIC_H_SIZE));
+      const keys: string[] = Object.keys(functions);
 
-      Object.keys(functions).forEach((funcSign: string) => {
-        this.generateFunctionBlock(contractTags, funcSign, functions[funcSign]);
-      });
+      if (keys.length > 0) {
+        contractTags.push(this.createHeaderTag("Functions info", TOPIC_H_SIZE));
+
+        keys.forEach((funcSign: string) => {
+          this.generateFunctionBlock(contractTags, funcSign, functions[funcSign]);
+        });
+      }
     }
 
     return json2md(contractTags);
   }
 
   generateFunctionBlock(contractTags: any[], funcSign: string, funcInfo: FunctionInfo) {
-    contractTags.push(this.createHeaderTag(`${funcInfo.name} function`, FUNCTION_NAME_H_SIZE));
-
-    const fullFuncSign = `${funcSign} ${funcInfo.stateMutability}`;
-    contractTags.push(
-      this.createParagraphTag(`${this.getBoldStr("Function signature:")} ${this.getInlineCodeStr(fullFuncSign)}`)
-    );
+    contractTags.push(this.createHeaderTag(`${funcInfo.name} (0x${funcInfo.selector})`, FUNCTION_NAME_H_SIZE));
 
     contractTags.push(
-      this.createParagraphTag(
-        `${this.getBoldStr("Function selector:")} ${this.getInlineCodeStr(`0x${funcInfo.selector}`)}`
-      )
+      this.createCodeTag([`function ${funcSign} ${this.getFunctionModifiers(funcInfo.stateMutability)}`])
     );
 
     this.generateBaseDescriptionBlock(contractTags, funcInfo);
 
     if (funcInfo.params) {
-      contractTags.push(this.createHeaderTag("Parameters description", FUNCTION_INSIDE_H_SIZE));
+      contractTags.push(this.createParagraphTag("Parameters:"));
 
       this.generateParamsBlock(contractTags, funcInfo.params);
     }
 
     if (funcInfo.returns) {
-      contractTags.push(this.createHeaderTag("Returns description", FUNCTION_INSIDE_H_SIZE));
+      contractTags.push(this.createParagraphTag("Return values:"));
 
       this.generateReturnsBlock(contractTags, funcInfo.returns);
     }
@@ -99,14 +107,12 @@ class MDGenerator {
   generateEventBlock(contractTags: any[], eventSign: string, eventInfo: EventInfo) {
     contractTags.push(this.createHeaderTag(`${eventInfo.name} event`, FUNCTION_NAME_H_SIZE));
 
-    contractTags.push(
-      this.createParagraphTag(`${this.getBoldStr("Event signature:")} ${this.getInlineCodeStr(eventSign)}`)
-    );
+    contractTags.push(this.createCodeTag([`event ${eventSign}`]));
 
     this.generateBaseDescriptionBlock(contractTags, eventInfo);
 
     if (eventInfo.params) {
-      contractTags.push(this.createHeaderTag("Parameters description", FUNCTION_INSIDE_H_SIZE));
+      contractTags.push(this.createParagraphTag("Parameters:"));
 
       this.generateParamsBlock(contractTags, eventInfo.params);
     }
@@ -115,48 +121,44 @@ class MDGenerator {
   generateErrorBlock(contractTags: any[], errorSign: string, errorInfo: ErrorInfo) {
     contractTags.push(this.createHeaderTag(`${errorInfo.name} error`, FUNCTION_NAME_H_SIZE));
 
-    contractTags.push(
-      this.createParagraphTag(`${this.getBoldStr("Error signature:")} ${this.getInlineCodeStr(errorSign)}`)
-    );
+    contractTags.push(this.createCodeTag([`error ${errorSign}`]));
 
     this.generateBaseDescriptionBlock(contractTags, errorInfo);
 
     if (errorInfo.params) {
-      contractTags.push(this.createHeaderTag("Parameters description", FUNCTION_INSIDE_H_SIZE));
+      contractTags.push(this.createParagraphTag("Parameters:"));
 
       this.generateParamsBlock(contractTags, errorInfo.params);
     }
   }
 
   generateParamsBlock(contractTags: any[], params: Param[]) {
-    const paramsDesc: string[] = params.map((param: Param) => {
-      let isIndexed: string = "";
+    const raws: string[][] = [];
 
-      if (param.isIndexed) {
-        isIndexed = " (indexed)";
-      }
-
-      return `${param.paramName}${isIndexed}: ${param.paramDescription}`;
+    params.map((param: Param) => {
+      raws.push([param.paramName, param.paramDescription]);
     });
 
-    contractTags.push(this.createUlTag(paramsDesc));
+    contractTags.push(this.createTableTag(["Name", "Description"], raws));
   }
 
   generateReturnsBlock(contractTags: any[], returns: Return[]) {
-    const returnsDesc: string[] = returns.map((returnEl: Return) => {
-      return `${returnEl.returnName}: ${returnEl.returnDescription}`;
+    const raws: string[][] = [];
+
+    returns.map((returnEl: Return) => {
+      raws.push([returnEl.returnName, returnEl.returnDescription]);
     });
 
-    contractTags.push(this.createUlTag(returnsDesc));
+    contractTags.push(this.createTableTag(["Name", "Description"], raws));
   }
 
   generateBaseDescriptionBlock(contractTags: any[], baseInfo: BaseDescription) {
     if (baseInfo.notice) {
-      contractTags.push(this.createParagraphTag(`${this.getBoldStr("Description:")} ${baseInfo.notice}`));
+      contractTags.push(this.createParagraphTag(baseInfo.notice));
     }
 
     if (baseInfo.details) {
-      contractTags.push(this.createParagraphTag(`${this.getBoldStr("Dev description:")} ${baseInfo.details}`));
+      contractTags.push(this.createParagraphTag(baseInfo.details));
     }
   }
 
@@ -166,6 +168,19 @@ class MDGenerator {
 
   getBoldStr(str: string): string {
     return `**${str}**`;
+  }
+
+  getFunctionModifiers(stateMutability: string): string {
+    switch (stateMutability) {
+      case "payable":
+        return "external payable";
+      case "nonpayable":
+        return "external";
+      case "view":
+        return "external view";
+      default:
+        throw new Error(`Failed to get function modifiers from ${stateMutability}`);
+    }
   }
 
   private createHeaderTag(headerContent: string, headerSize: number): any {
@@ -193,6 +208,34 @@ class MDGenerator {
 
   private createUlTag(ulContent: string[]): any {
     return { ul: ulContent };
+  }
+
+  private createTableTag(
+    headers: string[],
+    rows: string[][],
+    aligns: AlignTypes[] = [],
+    isPretty: boolean = true
+  ): any {
+    rows.forEach((row: string[]) => {
+      if (row.length != headers.length) {
+        throw new Error(
+          `Failed to create Table tag. Expected ${headers.length} columns, actual - ${row.length} columns in ${row} row`
+        );
+      }
+    });
+
+    const alignments: string[] = aligns.map((_, index: number) => {
+      return aligns && aligns[index] ? aligns[index] : DEFAULT_TABLE_ALIGN;
+    });
+
+    return {
+      table: {
+        headers: headers,
+        rows: rows,
+        pretty: isPretty,
+        aligns: alignments,
+      },
+    };
   }
 
   private createCodeTag(codeContent: string[], language: string = DEFAULT_CODE_LANGUAGE): any {
