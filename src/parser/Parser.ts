539862ca4e4ebff0b2a2b61a7b56dd9ded224d04
Ruslan Kasheparov <ruslankasheparov@gmail.com>
2023-05-25 19:35:19 +0200

    show internal functions for 'internal' libreries

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -48,6 +48,14 @@ export class Parser {
       throw new Error(`Contract ${name} not found in ${source}`);
     }
 
+    const allFunctions: FunctionDefinition[] = [...findAll("FunctionDefinition", contractNode)];
+    let functions: FunctionDefinition[];
+    if (contractNode.contractKind === "library" && allFunctions.every((fn) => fn.visibility === "internal")) {
+      functions = allFunctions.filter((node) => this.isPublicOrExternalOrInternal(node));
+    } else {
+      functions = allFunctions.filter((node) => this.isPublicOrExternal(node));
+    }
+
     const contractInfo: ContractInfo = {
       name: contractNode.name,
       isAbstract: contractNode.abstract,
@@ -72,10 +80,7 @@ export class Parser {
           STATE_VARIABLES_BLOCK_NAME
         ),
         this.parseDocumentation([...findAll("ModifierDefinition", contractNode)], MODIFIERS_BLOCK_NAME),
-        this.parseDocumentation(
-          [...findAll("FunctionDefinition", contractNode)].filter((node) => this.isPublicOrExternal(node)),
-          FUNCTIONS_BLOCK_NAME
-        ),
+        this.parseDocumentation(functions, FUNCTIONS_BLOCK_NAME),
       ],
     };
 
@@ -84,6 +89,10 @@ export class Parser {
 
   isPublicOrExternal(node: FunctionDefinition | VariableDeclaration): boolean {
     return node.visibility === "public" || node.visibility === "external";
+  }
+
+  isPublicOrExternalOrInternal(node: FunctionDefinition | VariableDeclaration): boolean {
+    return this.isPublicOrExternal(node) || node.visibility === "internal";
   }
 
   parseSelector(node: FunctionDefinition | VariableDeclaration): string {
